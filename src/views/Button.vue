f69b119415dcf1d97d77321166df6abb5672f02d
HyoungKi Kim <hkkim@gemiso.com>
2020-08-13 13:21:25 +0900

    최초 커밋

---
+++ src/views/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is an buttons page</h1>
+    <div class="flex justify-center">
+      <button class="btn m-2">Default Button</button>
+      <button class="btn btn-primary m-2">Primary Button</button>
+      <button class="btn btn-secondary m-2">Secondary Button</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Buttons",
+  components: {}
+};
+</script>

6a2e2e4106be76cc3c9d0cd027fb9c679f132703
Hyobin ju <hbju@gemiso.com>
2020-08-14 13:28:13 +0900

    ToggleButton 완료

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- bg-gray-300 shadow-inner-->
+  <button
+    class="rounded-full p-2 shadow-lg inline-block cursor-pointer m-2 focus:outline-none"
+    :class="checkedStyle"
+    @click="handleClick"
+  >
+    <slot name="icon"></slot>
+    <span v-if="textValue" class="pl-1">{{ textValue }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    defaultState: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      checked: this.defaultState || false,
+      checkedStyle: '',
+      textValue: this.text,
+    };
+  },
+
+  created() {
+    this.applyCheckedStyle();
+  },
+
+  methods: {
+    handleClick() {
+      this.checked = !this.checked;
+      this.applyCheckedStyle();
+    },
+    applyCheckedStyle() {
+      if (this.checked) {
+        this.checkedStyle = 'shadow-inner bg-gray-300 ';
+      } else {
+        this.checkedStyle = '';
+      }
+    },
+  },
+};
+</script>
